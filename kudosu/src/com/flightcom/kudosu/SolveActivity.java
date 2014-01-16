84f23b2bc33af85c08d9c6989463bd64489a30b3
sebastienmoreau <flightcom@wanadoo.fr>
2014-01-17 00:51:14 +0100

    20140116H0051

--- kudosu/src/com/flightcom/kudosu/SolveActivity.java
+++ kudosu/src/com/flightcom/kudosu/SolveActivity.java
@@ -1,26 +1,59 @@
 package com.flightcom.kudosu;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Camera;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class SolveActivity extends Activity {
 	
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private Camera mCamera;
-    
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_solve);
-		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-    	Toast.makeText(getApplicationContext(), "Take a picture of your grid", Toast.LENGTH_SHORT).show();
-	}
+    private CameraPreview mPreview;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solve);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+        
+    }
+    
+/*    private void drawGrid(){
+    	
+		// On récupère les dimensions de l'écran
+		DisplayMetrics displaymetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+		int width = displaymetrics.widthPixels;
+		int height = displaymetrics.heightPixels;
+		
+		Paint p = new Paint();
+		p.setColor(Color.BLACK);
+		Bitmap b = Bitmap.createBitmap(width-50, height-50, Bitmap.Config.ARGB_8888);
+        //Canvas c = new Canvas(b);
+        Canvas c = (RelativeLayout) findViewById(R.id.canvas);
+        c.drawLine(50, 50, 100, 100, p);
+        DrawCanvas dc = new DrawCanvas
+    }*/
+    
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 	    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
@@ -35,4 +68,26 @@ public class SolveActivity extends Activity {
 	    }
 	}
 
+	/** Check if this device has a camera */
+	private boolean checkCameraHardware(Context context) {
+	    if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+	        // this device has a camera
+	        return true;
+	    } else {
+	        // no camera on this device
+	        return false;
+	    }
+	}
+
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
+	}
 }
